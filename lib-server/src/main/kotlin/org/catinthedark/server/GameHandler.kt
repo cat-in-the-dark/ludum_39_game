c91f6b0e5e849d7f2e40d6bccc6f452db54a3191
Ilya Siganov <ilya.blan4@gmail.com>
2017-07-29 21:52:19 +0600

    dix bug with tcp server

--- lib-server/src/main/kotlin/org/catinthedark/server/GameHandler.kt
+++ lib-server/src/main/kotlin/org/catinthedark/server/GameHandler.kt
@@ -5,7 +5,6 @@ import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.SimpleChannelInboundHandler
 import org.catinthedark.shared.event_bus.BusRegister
 import org.catinthedark.shared.event_bus.EventBus
-import org.catinthedark.shared.event_bus.Handler
 import org.catinthedark.shared.invokers.Invoker
 import org.slf4j.LoggerFactory
 
@@ -16,9 +15,9 @@ import org.slf4j.LoggerFactory
  * You can send a message to other clients by sending [TCPMessage] via [EventBus]
  */
 class GameHandler(
-        private val invoker: Invoker
+        private val invoker: Invoker,
+        private val channels: MutableMap<String, Channel>
 ) : SimpleChannelInboundHandler<Any>() {
-    private val channels: MutableMap<String, Channel> = hashMapOf()
     private val log = LoggerFactory.getLogger(this::class.java)
 
     override fun handlerAdded(ctx: ChannelHandlerContext?) {
@@ -38,7 +37,7 @@ class GameHandler(
 
     override fun channelRegistered(ctx: ChannelHandlerContext) {
         val id = ctx.channel().id().asLongText()
-        channels.putIfAbsent(id, ctx.channel())
+        channels.put(id, ctx.channel())
         EventBus.send("GameHandler#channelRegistered", invoker, OnClientConnected(
                 ctx.channel().remoteAddress(),
                 id
@@ -48,38 +47,11 @@ class GameHandler(
 
     override fun channelUnregistered(ctx: ChannelHandlerContext) {
         val id = ctx.channel().id().asLongText()
-        channels.remove(id)
         EventBus.send("GameHandler#channelUnregistered", invoker, OnClientDisconnected(
                 ctx.channel().remoteAddress(),
                 id
         ))
+        channels.remove(id)
         super.channelUnregistered(ctx)
-    }
-
-    @Handler
-    fun send(msg: TCPMessage) {
-        if (msg.to == null) {
-            // will send to all
-            channels.forEach { _, ch ->
-                send(msg, ch)
-            }
-        } else {
-            val ch = channels.getOrDefault(msg.to, null)
-            send(msg, ch)
-        }
-    }
-
-    private fun send(msg: TCPMessage, ch: Channel?) {
-        if (ch == null) {
-            log.warn("There is no channel with id ${msg.to}")
-            return
-        }
-        log.debug("Sending $msg To ${ch.id()} ${ch.remoteAddress()}")
-
-        if (ch.isActive && ch.isOpen && ch.isWritable) {
-            ch.writeAndFlush(msg.payload)
-        } else {
-            log.warn("Channel with id ${msg.to} ${ch.remoteAddress()} is not ready to receive messages.")
-        }
     }
 }
