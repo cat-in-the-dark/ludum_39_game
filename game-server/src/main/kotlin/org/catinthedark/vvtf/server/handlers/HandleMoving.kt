735e37d26ce88849f4171f0e00a5671a477df634
Denis Nelubin <dnelubin@gmail.com>
2017-07-30 13:14:09 +0600

    Added handling of movements on server side.

--- game-server/src/main/kotlin/org/catinthedark/vvtf/server/handlers/HandleMoving.kt
+++ game-server/src/main/kotlin/org/catinthedark/vvtf/server/handlers/HandleMoving.kt
@@ -1,7 +1,27 @@
 package org.catinthedark.vvtf.server.handlers
 
+import org.catinthedark.vvtf.shared.Const
 import org.catinthedark.vvtf.shared.messages.Player
+import org.catinthedark.vvtf.shared.messages.isAttacking
+import org.catinthedark.vvtf.shared.messages.isJumping
+import org.catinthedark.vvtf.shared.messages.isMoving
 import org.catinthedark.vvtf.shared.models.PlayerParams
 
 fun handleMoving(player: Player, params: PlayerParams, delta: Long) {
+    with(player) {
+        if (x > lastX) {
+            angle = 0f
+        }
+        if (x < lastX) {
+            angle = 180f
+        }
+        if (!isJumping() && !isAttacking()) {
+            if (isMoving()) {
+                state = Const.PlayerState.walking.name
+            } else {
+                state = Const.PlayerState.idle.name
+            }
+        }
+        lastX = x
+    }
 }
